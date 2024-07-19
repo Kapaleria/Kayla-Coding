0f31a76bfd91da3fdd5fb5e96105032f978e3b36
irene-nakabugo <irenenakabugo0@gmail.com>
2024-07-19 14:07:03 +0300

    if else statements

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
  
  const yoghurt = 'strawberry';
  const number = 90;
+
+
+//  IF STATEMENTS (Decision - making statement)
+/**if(condition){
+ * block of code to be executed if found true
+}
+else{ everything that is run if at all the condition is false
+}
+*/
