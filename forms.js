fd34d5b16eedf0819c126edbf7311c72f0fabb36
irene-nakabugo <irenenakabugo0@gmail.com>
2024-08-02 15:26:39 +0300

    create server and connect to DB

---
+++ forms.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require ('mongoose')
+
+const app = express()
+
+const port = 4000;
+
+mongoose.connect('mongodb+srv://Irene:irene@cluster0.fumaixk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>console.log('connected to the DB'))
+.catch(err=>{console.log('encountered error', err)})
+
+// app.listen(`${port}`)
+
+app.listen (port, ()=>console.log('CONNECTED TO PORT'))
