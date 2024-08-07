03e6aba065c7537d0f666e95bb91ec20d39ac47c
irene-nakabugo <irenenakabugo0@gmail.com>
2024-08-07 14:56:29 +0300

    Create a database schema

---
+++ schema.js
@@ -0,0 +1,18 @@
+// import mongoose
+const mongoose = require ('mongoose')
+
+const orderSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+
+    email:{
+        type:String
+    },
+
+    phoneNumber:{
+        type:String,
+        required:true
+    }
+})
