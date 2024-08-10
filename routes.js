79e0888a1f2c453c52af118a81d378d4e8e3ae24
irene-nakabugo <irenenakabugo0@gmail.com>
2024-08-10 14:05:21 +0300

    POST METHOD

--- routes.js
+++ routes.js
@@ -2,6 +2,25 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get ('/', ()=>{
+const orders = require('./schema')
+
+//GET method
+router.get ('/', (req,res)=>{
     res.json({mssg:'Hello'})
 })
+
+//POST method
+router.post('/orders', (req,res)=>{
+  const order = new orders({
+    name:req.body.name,
+    email: req.body.email,
+    phoneNumber:req.body.phoneNumber
+  })
+  order.save()
+        .then(data=>{res.json(data)})
+        .catch(err=>{res.json({mss:err})})
+})
+
+//asynchronous approach
+
+module.exports = router
