ccae5859760f3b1687e7143a60507836f5cef555
irene-nakabugo <irenenakabugo0@gmail.com>
2024-08-26 16:13:05 +0300

    Update forms.js

--- forms.js
+++ forms.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mongoose = require ('mongoose')
+const cors = require ('cors')
 require('dotenv/config')
 
 const routes = require('./routes')
 const bodyParser = require('body-parser')
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 app.use('/api', routes)
 
